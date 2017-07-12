ab966d6677de9bc258acce2a51eb8b1d17370e6a
krzychu124 <krzychu124@gmail.com>
2017-07-12 20:30:36 +0200

    dodano klase NewClass

---
+++ src/pl/sdacademy/exceptions/NewClass.java
@@ -0,0 +1,22 @@
+package pl.sdacademy.exceptions;
+
+/**
+ * Created by RENT on 2017-07-12.
+ */
+public class NewClass {
+    private int a;
+
+    public NewClass(int a) {
+
+        this.a = a;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+}
